f7f617046b2a1ce37be22738d6d64adb85b75b91
hanmiton <hanmilton_12@outlook.com>
2017-05-05 11:33:22 -0500

    cambiando de peurto a 80

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 80;
+var port =  80;
 var morgan = require('morgan');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
