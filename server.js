a0951a16d5f018be5d2a4dfadc44d75779b36226
hanmiton <hanmilton_12@outlook.com>
2017-05-05 10:52:21 -0500

    cambiando a puerto 80

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 80;
 var morgan = require('morgan');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
